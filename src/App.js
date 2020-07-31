69503d03900236b04402d16b810b11f512f38875
Sinharitik589 <sinharitik18112835@gmail.com>
2020-07-31 16:48:24 +0530

    layout ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,235 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <nav className="landing-navbar"></nav>
+        <div className="container">
+          <div className="landing-header">
+            <h1>CREATING AN ECOSYSTEM OF OPPORTUNITIES</h1>
+            <p
+              className="landing-para"
+              style={{
+                color: "#565E81",
+                fontFamily: "Heebo, 'sans-serif'",
+                fontSize: "22px",
+                fontWeight: 500,
+                lineHeight: "30px",
+              }}
+            >
+              Recent graduates want great jobs. Companies want great talents.
+              Colleges and Universities want great number of opportunities for
+              their students.Scholarly science enables collaboration between
+              companies,universities and university through various unique and
+              innovative way.
+            </p>
+            <a href="" className="landing-button">
+              Get Started
+            </a>
+            <img className="main-image" src="new image.png" />
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="landing-components">
+                <img src="component.svg" style={{ marginBottom: 16 }} />
+                <h4>Better Components</h4>
+
+                <p className="landing-component-des">
+                  A pseudo-Latin text used in web design, layout, and printing
+                  in place of things to emphasise design for previewing layouts.
+                </p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div
+            className="landing-header"
+            style={{ marginBottom: "0", paddingBottom: 35 }}
+          >
+            <h1>Start working in seconds</h1>
+            <p
+              className="landing-para"
+              style={{
+                color: "#565E81",
+                fontFamily: "Heebo, 'sans-serif'",
+                fontSize: "22px",
+                fontWeight: 500,
+                lineHeight: "30px",
+                marginBottom: 0,
+                textAlign: "center",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore aliqua.
+            </p>
+          </div>
+        </div>
+        <div
+          className="row"
+          style={{
+            display: "flex",
+            flexDirection: "row-reverse",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <div className="col-md-7 col-sm-12">
+            <img src="feature1.png" style={{ width: "100%", height: "auto" }} />
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <h3
+              style={{ fontSize: 30, lineHeight: "42px", textAlign: "center" }}
+            >
+              Origins and discoveries
+            </h3>
+            <p
+              style={{
+                color: "#565E81",
+                fontFamily: "Heebo, 'sans-serif'",
+                fontSize: "22px",
+                fontWeight: 500,
+                lineHeight: "30px",
+                marginBottom: 0,
+                textAlign: "center",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div
+          className="row"
+          style={{
+            alignItems: "center",
+          }}
+        >
+          <div className="col-md-7 col-sm-12">
+            <img src="feature1.png" style={{ width: "100%", height: "auto" }} />
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <h3
+              style={{ fontSize: 30, lineHeight: "42px", textAlign: "center" }}
+            >
+              Origins and discoveries
+            </h3>
+            <p
+              style={{
+                color: "#565E81",
+                fontFamily: "Heebo, 'sans-serif'",
+                fontSize: "22px",
+                fontWeight: 500,
+                lineHeight: "30px",
+                textAlign: "center",
+                marginBottom: 0,
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+        <div
+          className="row"
+          style={{
+            display: "flex",
+            flexDirection: "row-reverse",
+            alignItems: "center",
+          }}
+        >
+          <div className="col-md-7 col-sm-12">
+            <img src="feature1.png" style={{ width: "100%", height: "auto" }} />
+          </div>
+          <div className="col-md-4 col-sm-12">
+            <h3
+              style={{ fontSize: 30, lineHeight: "42px", textAlign: "center" }}
+            >
+              Origins and discoveries
+            </h3>
+            <p
+              style={{
+                color: "#565E81",
+                fontFamily: "Heebo, 'sans-serif'",
+                fontSize: "22px",
+                fontWeight: 500,
+                lineHeight: "30px",
+                marginBottom: 0,
+                textAlign: "center",
+              }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
